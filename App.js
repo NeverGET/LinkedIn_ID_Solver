167a9c4316f27738f4eae133a524b089aeaaaa48
neverget <cem_al_96@hotmail.com>
2022-11-28 14:16:45 +0300

    Code Optimised.

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 //const express = require('express');
 import express from "express";
-import check_id from "./check_id.js";
+import Check_ID from "./check_id.js";
 
 export default class App extends express {
     constructor() {
         super();
 
-        this.check_id = new check_id();
+        //this.check_id = new Check_ID();
+        //this.check_id.setRoutes();
         this.use('/check_id', this.check_id);
         this.port = process.env.PORT || 5500;
         this.listen(this.port, () => console.log(`Listening on Port: ${this.port}`));
     }
+    check_id = new Check_ID();
 }
--- check_id.js
+++ check_id.js
@@ -3,50 +3,52 @@ import axios from "axios";
 import {parse} from "node-html-parser"
 import {unescape} from 'html-escaper';
 
-export default class check_id extends express.Router {
+export default class Check_ID extends express.Router {
     constructor(props) {
         super(props);
         this.linkedInLinks = {industry: "https://learn.microsoft.com/en-us/linkedin/shared/references/reference-tables/industry-codes"}
-
-        this.findByID = async function (id, url) {
-            let htmlObject;
-            await axios.get(url).then(function (response) {
-                htmlObject = parse(response.data);
-            });
-            let returnVal = Array.prototype.slice.call(htmlObject.querySelectorAll('table > tbody > tr')).find((value) => {
-                let elements = value.querySelectorAll('td');
-                if (id === elements[0].rawText) {
+        this.setRoutes();
+    }
+    setRoutes = ()=>{
+        this.get(`/`, this.#getCallback().bind(this));
+    }
+    #getCallback = ()=>{
+        return function (req, res) {
+            let keys = req.url.split("?")[1].split("&").filter(value => {
+                if (value.indexOf("=") !== -1) {
                     return true;
                 }
+            });
+            keys = keys.map(value => {
+                let key = value.split("=");
+                return {key: key[0], value: key[1]};
             })
+            let idPromises = keys.map((value) => {
+                return this.#findByID(value.value, this.linkedInLinks[value.key]).then(value1 => {
+                    return value1;
+                });
+            }, this);
+            Promise.all(idPromises).then(results => {
+                res.status(200).json(results);
+            });
 
-            return returnVal !== undefined ? {id: id, value: unescape(returnVal.querySelectorAll('td')[2].rawText)} : {
-                id: id, value: "-1"
-            };
         }
-        this.callback = function (self) {
-            return function (req, res) {
-                let keys = req.url.split("?")[1].split("&").filter(value => {
-                    if (value.indexOf("=") !== -1) {
-                        return true;
-                    }
-                });
-                keys = keys.map(value => {
-                    let key = value.split("=");
-                    return {key: key[0], value: key[1]};
-                })
-                let idPromises = keys.map(value => {
-                    return self.findByID(value.value, self.linkedInLinks[value.key]).then(value1 => {
-                        return value1;
-                    });
-                })
-                Promise.all(idPromises).then(results => {
-                    res.status(200).json(results);
-                });
-
+    }
+    #findByID = async (id, url) => {
+        let htmlObject;
+        await axios.get(url).then(function (response) {
+            htmlObject = parse(response.data);
+        });
+        let returnVal = Array.prototype.slice.call(htmlObject.querySelectorAll('table > tbody > tr')).find((value) => {
+            let elements = value.querySelectorAll('td');
+            if (id === elements[0].rawText) {
+                return true;
             }
-        }
-        this.get(`/`, this.callback(this));
+        })
+
+        return returnVal !== undefined ? {id: id, value: unescape(returnVal.querySelectorAll('td')[2].rawText)} : {
+            id: id, value: "-1"
+        };
     }
 
 
